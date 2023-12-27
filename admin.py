75613e1f173fad2534954000e1a4c40768c5274e
swamypotharaveni <46279448+swamypotharaveni@users.noreply.github.com>
2023-12-27 21:53:03 +0530

    Delete admin.py

--- admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from myapp1.models import CustomUser
-from django.contrib.admin import AdminSite
-from django.contrib.auth.models import User,Group
-
-
-admin.site.register(CustomUser)
-admin.site.unregister(Group)
-# Register your models here.
